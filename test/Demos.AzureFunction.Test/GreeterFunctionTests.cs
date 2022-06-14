1abb95953b3f0a578fd805ebd76d62535dae8886
bhavani.velivala <bhavani.velivala@servicenow.com>
2022-06-14 17:06:27 +0530

    initial changes

---
+++ test/Demos.AzureFunction.Test/GreeterFunctionTests.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using Moq;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Internal;
+using System.Collections.Generic;
+
+namespace DotNetSample.Test
+{
+    public class GreeterFunctionTests
+    {
+        private ILogger logger;
+
+        private HttpRequest request;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.logger = Mock.Of<ILogger>();
+            this.request = Mock.Of<HttpRequest>();
+        }
+
+        [Test]
+        public async Task Test_JsonRequestBody_ReturnsOkObjectResult()
+        {
+            // Arrange
+            using var stream =
+                new MemoryStream(
+                    System.Text.Encoding.UTF8.GetBytes("{\"name\":\"jan\"}")
+                );
+
+            Mock.Get(request)
+                .SetupGet(r => r.Body)
+                .Returns(stream);
+
+            // Act
+            var response = await GreeterFunction
+                .ExecuteAsync(request, logger)
+                .ConfigureAwait(false);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(response);
+        }
+    }
+}
